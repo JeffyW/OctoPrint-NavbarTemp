681c761bfc9cee0c3a54f98290e4a338b177f54d
Jeff Wight <JeffyW@gmail.com>
2017-06-16 07:13:36 -0700

    Remove temperature

--- octoprint_navbartemp/__init__.py
+++ octoprint_navbartemp/__init__.py
@@ -16,87 +16,7 @@ class NavBarPlugin(octoprint.plugin.StartupPlugin,
                    octoprint.plugin.SettingsPlugin):
 
     def __init__(self):
-        self.isRaspi = False
-        self.isSOC = False
-        self.debugMode = False      # to simulate temp on Win/Mac
         self.displayRaspiTemp = True
-        self._checkTempTimer = None
-
-    def on_after_startup(self):
-        self.displayRaspiTemp = self._settings.get(["displayRaspiTemp"])
-        self._logger.debug("displayRaspiTemp: %s" % self.displayRaspiTemp)
-
-        if self.debugMode:
-            self.isRaspi = True
-            if self.displayRaspiTemp:
-                self.startTimer(5.0)
-        elif sys.platform == "linux2":
-            with open('/proc/cpuinfo', 'r') as infile:
-                    cpuinfo = infile.read()
-            # Match a line like 'Hardware   : BCM2709'
-            match = re.search('^Hardware\s+:\s+(\w+)$', cpuinfo, flags=re.MULTILINE | re.IGNORECASE)
-            self._logger.debug("Hardware: %s", match.group(1))
-
-            if match is None:
-                # Couldn't find the hardware, assume it isn't a pi.
-                self.isRaspi = False
-            elif match.group(1) == 'BCM2708':
-                self._logger.debug("Pi 1")
-                self.isRaspi = True
-            elif match.group(1) == 'BCM2709':
-                self._logger.debug("Pi 2")
-                self.isRaspi = True
-            elif match.group(1) == 'sun50iw1p1':
-                self._logger.debug("Pine A64")
-                self.isSOC = True
-
-            if (self.isRaspi or self.isSOC) and self.displayRaspiTemp:
-                self._logger.debug("Let's start RepeatedTimer!")
-                self.startTimer(30.0)
-
-        self._logger.debug("is Raspberry Pi? - %s" % self.isRaspi)
-
-    def startTimer(self, interval):
-        self._checkTempTimer = RepeatedTimer(interval, self.checkRaspiTemp, None, None, True)
-        self._checkTempTimer.start()
-
-    def checkRaspiTemp(self):
-        self._logger.info("Checking Raspberry Pi internal temperature")
-
-        if self.debugMode:
-            import random
-            def randrange_float(start, stop, step):
-                return random.randint(0, int((stop - start) / step)) * step + start
-            p = "temp=%s'C" % randrange_float(5, 60, 0.1)
-
-        elif sys.platform == "linux2":
-            if self.isRaspi:
-                from sarge import run, Capture
-                p = run("/opt/vc/bin/vcgencmd measure_temp", stdout=Capture())
-                if p.returncode==1:
-                    self.isRaspi = False
-                    self._logger.error("SoC temperature not found.")
-                else:
-                    p = p.stdout.text
-                    self._logger.debug("response from sarge: %s" % p)
-                    match = re.search('=(.*)\'', p)
-
-            elif self.isSOC:
-                self._logger.debug("Reading /sys/devices/virtual/thermal/thermal_zone0/temp")
-                with open('/sys/devices/virtual/thermal/thermal_zone0/temp', 'r') as content_file:
-                    p = content_file.read()
-                self._logger.debug("Read: %s" % p)
-                match = re.search('(\d+)', p)
-
-        if not match:
-            self._logger.error("Invalid temperature format.")
-            self.isRaspi = False
-            self.isSOC = False
-        else:
-            temp = match.group(1)
-            self._logger.debug("Temperature: %s" % temp)
-            self._plugin_manager.send_plugin_message(self._identifier, dict(israspi=self.isRaspi, issoc=self.isSOC, raspitemp=temp))
-
 
 	##~~ SettingsPlugin
     def get_settings_defaults(self):
@@ -106,17 +26,6 @@ class NavBarPlugin(octoprint.plugin.StartupPlugin,
         octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
 
         self.displayRaspiTemp = self._settings.get(["displayRaspiTemp"])
-
-        if self.displayRaspiTemp:
-            interval = 5.0 if self.debugMode else 30.0
-            self.startTimer(interval)
-        else:
-            if self._checkTempTimer is not None:
-                try:
-                    self._checkTempTimer.cancel()
-                except:
-                    pass
-            self._plugin_manager.send_plugin_message(self._identifier, dict())
 
 	##~~ TemplatePlugin API
     def get_template_configs(self):
